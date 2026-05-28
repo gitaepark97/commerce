8b54951d0448b4d2717473036d2f3a76dd184fc3
gitaepark97 <parkkitae7@gmail.com>
2026-05-28 20:42:43 +0900

    Initial commit

---
+++ src/test/java/com/hugo/commerce/TestCommerceApplication.java
@@ -0,0 +1,11 @@
+package com.hugo.commerce;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CommerceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
